c5da2f23c547641347a579d5d0373bb6d1066d1b
zqm64 <zqm64@outlook.com>
2020-06-19 19:03:38 +0800

    j7lte-common: Move Inherit from common msm8916 to top of makefile
    Follow with j5-common
    
    Change-Id: Ie76397e7e3c37c6b14e9805a0c70670e7478ffa5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit from common msm8916
+include device/samsung/msm8916-common/BoardConfigCommon.mk
+
 LOCAL_PATH := device/samsung/j7lte-common
 
 # Bluetooth
@@ -37,6 +40,3 @@ BOARD_PROVIDES_LIBRIL := true
 
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true
-
-# Inherit from common msm8916
-include device/samsung/msm8916-common/BoardConfigCommon.mk
